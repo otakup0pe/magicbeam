38b6721e8a4a5a2af5ee242078c1ea57552e1248
Jonathan Freedman <jfreedman@exacttarget.com>
2011-10-24 13:32:35 -0700

    * use actual list instead of csv

--- include/magicbeam_config.hrl
+++ include/magicbeam_config.hrl
@@ -1,8 +1,8 @@
 -define(THUNDERBEAM_ENABLED, magicbeam_srv:cfgget(thunderbeam_enabled, false)).
 -define(THUNDERBEAM_WAIT_BASE, magicbeam_srv:cfgget(thunderbeam_wait_base, 60)).
 -define(THUNDERBEAM_WAIT_VARIABLE, magicbeam_srv:cfgget(thunderbeam_wait_variable, 5)).
--define(THUNDERBEAM_IMMUNE_PROC, magicbeam_srv:cfgget(thunderbeam_immune_proc, "")).
--define(THUNDERBEAM_IMMUNE_APP, magicbeam_srv:cfgget(thunderbeam_immune_app, "stdlib,kernel,mnesia,sasl,inets")).
+-define(THUNDERBEAM_IMMUNE_PROC, magicbeam_srv:cfgget(thunderbeam_immune_proc, [])).
+-define(THUNDERBEAM_IMMUNE_APP, magicbeam_srv:cfgget(thunderbeam_immune_app, [stdlib,kernel,mnesia,sasl,inets])).
 
 -define(HOTBEAM_ENABLED, magicbeam_srv:cfgget(hotbeam_enabled, true)).
 -define(HOTBEAM_COMPILE, magicbeam_srv:cfgget(hotbeam_compile, false)).
